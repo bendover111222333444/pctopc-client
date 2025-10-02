077fe7a31475720e1175a2a9a318e065ad2fe8ca
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2025-10-02 03:11:38 -0700

    added some code

--- screenSend.cs
+++ screenSend.cs
@@ -1,2 +1,32 @@
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
+namespace ScreenCapture
+{
+    class DisplayForm : Form
+    {
+        public CreateForm(short x, short y)
+        {
+            this.Text = "window";
+            this.Width = x;
+            this.Height = y;
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            CreateForm NewWindow = new CreateForm(100, 100);
+            Application.Run(NewWindow);
+        }
+    }
+
+    class Send
+    {
+
+
+
+    }
+}
