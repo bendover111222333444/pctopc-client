19d6e17d8e499eeeee49c5ae7b013e79b7225dd5
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-05-05 23:38:15 -0700

    do quick additions

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const clientButton = document.getElementById("clientButton");
-const serverButton = document.getElementById("pcButton")
 
 clientButton.addEventListener("click", function(){
 
     window.location.href = "./html/client.html";
-
-})
-
-serverButton.addEventListener("click", function(){
-
-    //window.location.href = "./html/client.html";
 
 })
